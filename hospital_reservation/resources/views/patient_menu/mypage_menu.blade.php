f2dff002f8c9df061dc3a50f17d92c4e940f48c7
mattuntun <mattuntun3@gmail.com>
2020-01-02 19:55:10 +0900

    マイページのビューで日付の表示を追加

--- hospital_reservation/resources/views/patient_menu/mypage_menu.blade.php
+++ hospital_reservation/resources/views/patient_menu/mypage_menu.blade.php
@@ -48,7 +48,8 @@
         <div class = "ResInfo">
             <ul>
                 <li style="font-size:40px; padding-bottom:30px;"><b>予約診療科:</b>{{$foreignReservationData->reservation_department}}</li>
-                <li style="font-size:40px;"><b>予約診療時間</b>　{{$foreignReservationData->reservation_time}}　より診療開始</li>
+                <li style="font-size:40px;"><b>予約日:</b>　{{$foreignReservationData->reservation_date}}</li>
+                <li style="font-size:40px;"><b>診療開始予定時間:</b>　{{$foreignReservationData->reservation_time}}　より診療開始</li>
                 
                 
                 {{-- タグ付ボタン(スモール) --}}
