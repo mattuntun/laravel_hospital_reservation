15b4012a9b3c498ef897bafebde2255495ca2d61
mattuntun <mattuntun3@gmail.com>
2020-01-09 17:58:58 +0900

    分を無視した状態での開診・休診・閉診をスケジュールへ反映

--- hospital_reservation/app/Schedule.php
+++ hospital_reservation/app/Schedule.php
@@ -16,16 +16,20 @@ class Schedule
 
 //テーブルのhtmlを作成
 $this->table = <<<EOF
-<table class="table table-bordered" style="background: white;>
+<table class="table table-bordered" align="center" style="background: white;" >
     <tr>
-        <th style="background: #AEC4E5; scope="col"></th>
-        <th style="background: #AEC4E5; text-align:center; width: 70%; font-size:30px;" scope="col">テストテーブル時間</th>
-        <th style="background: #AEC4E5; text-align:center; width: 30%; font-size:30px; scope="col">テストテーブル表示</th>
+        <th style="background: #AEC4E5; text-align:center; width: 70%; font-size:30px;" scope="col">タイムテーブル</th>
+        <th style="background: #AEC4E5; text-align:center; width: 30%; font-size:30px; scope="col">予約状況</th>
     </tr>
 EOF;
     //開院時と閉院時の設定(分は考えない)
-    $open_hour = substr($departmentDatas->start_time, 0, -6);
-    $close_hour = substr($departmentDatas->close_time, 0, -6);
+    $open_hour = substr($departmentDatas->start_time, 0, -6); //開院時間を整数へ
+    $close_hour = substr($departmentDatas->close_time, 0, -6); //閉院時間を整数へ
+    $break_start_hour = substr($departmentDatas->break_time_start, 0, -6); //休憩開始を整数
+    $break_finish_hour = substr($departmentDatas->break_time_close, 0, -6); //休憩終了を整数
+
+    $break_start = substr($departmentDatas->break_time_start, 0, -3); //休憩開始を時間表示
+    $break_finish = substr($departmentDatas->break_time_close, 0, -3); //休憩開始を時間表示
 
     //◎、〇、△の条件を呼び出し
     $doubleCircleReservationValue = 60;
@@ -39,8 +43,22 @@ EOF;
                     $belongMin =  $min+29;
                     $belongTime = $hour.":".$belongMin;
                     $this->table.="<tr>
-                                        <td>
-                                             <button type='submit' class='btn btn-lg btn-block' style='background: white; font-size:30px; text-align:center;' onclick='location.href=/mypage/complete_add_new_reservation>
+                                        <td align='center'>";
+                                        //休憩時間の表示設定
+                                    if( ($hour >= $break_start_hour) && ($hour < $break_finish_hour)){     
+                                        $this->table.="{$openTime}&#126;は休診時間</td>
+                                        <td align='center'>予約不可</td>";
+                                        
+                                        continue 2;
+                                       
+                                        }else{
+ 
+                                $this->table.="<button type='submit'
+                                                class='btn btn-lg btn-block'
+                                                style='background: white;
+                                                font-size:30px;
+                                                text-align:center;'
+                                                onclick='location.href=/mypage/complete_add_new_reservation>
                                             <input type='hidden' value = '$openTime' name = 'targetTime'>
                                             <input type='hidden' value ='$search_Department' name = 'clinical_department'>
                                             <input type='hidden' value ='$targetDate' name = 'targetDate'>
@@ -67,9 +85,10 @@ EOF;
                                             $this->table.="&#10005;";    // ✕
                                         }
                             $this->table.="</td>
-                                    </tr>";                
-                }    
-            }
+                                    </tr>";
+                                         } //ifのカッコ
+                }//for　$minのカッコ    
+            }//for　$hourのカッコ
 
         return $this->table .= '</table>';
     }
