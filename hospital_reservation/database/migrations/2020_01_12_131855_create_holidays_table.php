994244d4515f65b03ef871b3f1f59906a76439ae
mattuntun <mattuntun3@gmail.com>
2020-01-12 13:51:02 +0900

    隔週休みのカラム１～３を追加を微修正

--- hospital_reservation/database/migrations/2020_01_12_131855_create_holidays_table.php
+++ hospital_reservation/database/migrations/2020_01_12_131855_create_holidays_table.php
@@ -18,9 +18,9 @@ class CreateHolidaysTable extends Migration
             $table->string('clinical_department',50)->comment('診療科');
             $table->date('holiday_date')->comment('休日'); 
             $table->string('description')->comment('休日概要');
-            $table->string('weekly_holiday_1')->nullable()->comment('毎週の休み1(日曜日：0～土曜日6)');
-            $table->string('weekly_holiday_2')->nullable()->comment('毎週の休み2(日曜日：0～土曜日6)');
-            $table->string('weekly_holiday_3')->nullable()->comment('毎週の休み3(日曜日：0～土曜日6)');
+            $table->string('weekly_holiday_1',10)->nullable()->comment('毎週の休み1(日曜日：0～土曜日6)');
+            $table->string('weekly_holiday_2',10)->nullable()->comment('毎週の休み2(日曜日：0～土曜日6)');
+            $table->string('weekly_holiday_3',10)->nullable()->comment('毎週の休み3(日曜日：0～土曜日6)');
             $table->timestamps();
 
             $table->index('clinical_department');
