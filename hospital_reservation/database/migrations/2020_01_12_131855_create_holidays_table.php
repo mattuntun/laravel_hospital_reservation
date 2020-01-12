35d8180d3576cd0a0def1a8bfebf69ccd3c205d6
mattuntun <mattuntun3@gmail.com>
2020-01-12 13:49:41 +0900

    休日マイグレーションに隔週休みのカラム１～３を追加

--- hospital_reservation/database/migrations/2020_01_12_131855_create_holidays_table.php
+++ hospital_reservation/database/migrations/2020_01_12_131855_create_holidays_table.php
@@ -14,10 +14,13 @@ class CreateHolidaysTable extends Migration
     public function up()
     {
         Schema::create('holidays', function (Blueprint $table) {
-            $table->bigIncrements('id')->comment('主キー');
+            $table->increments('id')->comment('主キー');
             $table->string('clinical_department',50)->comment('診療科');
             $table->date('holiday_date')->comment('休日'); 
-            $table->string('description')->comment('休日概要');; 
+            $table->string('description')->comment('休日概要');
+            $table->string('weekly_holiday_1')->nullable()->comment('毎週の休み1(日曜日：0～土曜日6)');
+            $table->string('weekly_holiday_2')->nullable()->comment('毎週の休み2(日曜日：0～土曜日6)');
+            $table->string('weekly_holiday_3')->nullable()->comment('毎週の休み3(日曜日：0～土曜日6)');
             $table->timestamps();
 
             $table->index('clinical_department');
