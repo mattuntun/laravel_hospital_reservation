34e0f8ff1adc5a8ee05931c7ea2ec56b76bd5388
mattuntun <mattuntun3@gmail.com>
2020-01-12 13:32:24 +0900

    holidayマイグレーションのテーブル設定完了・診療科を外部キーに設定

--- hospital_reservation/database/migrations/2020_01_12_131855_create_holidays_table.php
+++ hospital_reservation/database/migrations/2020_01_12_131855_create_holidays_table.php
@@ -14,8 +14,20 @@ class CreateHolidaysTable extends Migration
     public function up()
     {
         Schema::create('holidays', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->bigIncrements('id')->comment('主キー');
+            $table->string('clinical_department',50)->comment('診療科');
+            $table->date('holiday＿date')->comment('休日');; 
+            $table->string('description'); 
             $table->timestamps();
+
+            $table->index('clinical_department');
+
+            $table->foreign('clinical_department')
+            ->references('clinical_department')
+            ->on('clinical_departments')
+            ->onDelete('cascade')
+            ->onUpdate('cascade');
+
         });
     }
 
