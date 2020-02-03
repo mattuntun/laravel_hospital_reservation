81913ae869c25372519355a40aee83f3096bed92
mattuntun <mattuntun3@gmail.com>
2020-02-03 09:28:49 +0900

    プルリク修正

--- hospital_reservation/app/HospitalCalendar.php
+++ hospital_reservation/app/HospitalCalendar.php
@@ -78,7 +78,7 @@ EOS;
                     // 先月・来月の日付の場合
                     $this->html .= "<td>&nbsp;</td>";
                 
-                } elseif ($i ==0 ) {   //隔週の休診日を追加する場合は "|| $i ==6"等を足す
+                } elseif ($i == 0 ) {   //隔週の休診日を追加する場合は "|| $i ==6"等を足す
                     $this->html .="<td style = color:#E9E9E9;>". $day . "</td>";
                 
                 } elseif ($day < $today) {
